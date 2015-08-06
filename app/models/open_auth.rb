0d1e2708ded8a7a0fb3303b06b5566186d370d17
lijinmin <lijinmin@xjgreat.com>
2015-08-06 19:27:50 +0800

    m

--- app/models/open_auth.rb
+++ app/models/open_auth.rb
@@ -1,6 +1,7 @@
 class OpenAuth < ActiveRecord::Base
   def self.init_open_auth
-    open_auth = OpenAuth.new
+    if OpenAuth.where(:id=>1).first == nil
+      open_auth = OpenAuth.new
       open_auth.authorization_uri = 'https://accounts.google.com/o/oauth2/auth'
       open_auth.token_credential_uri = 'https://accounts.google.com/o/oauth2/token'
       open_auth.redirect_uri = 'http://wxtest.aggso.com/oauth2callback'
@@ -8,15 +9,15 @@ class OpenAuth < ActiveRecord::Base
       open_auth.client_secret = 'VhUtzfBcK5s27z9rtkaH12Mz'
       open_auth.scope = 'https://www.google.com/m8/feeds/'
       open_auth.save
+    end
+
   end
   def self.get_auth_uri
     client = Oauth2.credentials
     auth_uri = (client.authorization_uri(options={})).to_s
   end
   def self.save_oauth_code(code)
-    if OpenAuth.where(id: 1).first == nil
-      OpenAuth.init_open_auth
-    end
+    OpenAuth.init_open_auth
     open_auth = OpenAuth.where(id: 1).first
     open_auth.code = code
     open_auth.save

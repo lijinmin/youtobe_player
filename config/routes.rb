c287545e38f072e03bb77ffbe977a13963d906b8
lijinmin <lijinmin@xjgreat.com>
2015-08-06 15:04:18 +0800

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :players
-  root 'oauth2callback' => 'players#index'
+  # root 'oauth2callback' => 'players#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

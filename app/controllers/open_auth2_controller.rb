80c8e62037336a3a02033fa773a90124f9f974ca
lijinmin <lijinmin@xjgreat.com>
2015-08-07 10:45:42 +0800

    m

--- app/controllers/open_auth2_controller.rb
+++ app/controllers/open_auth2_controller.rb
@@ -6,12 +6,12 @@ class OpenAuth2Controller < ApplicationController
   def new
     if OpenAuth2.where(:id=>1).first == nil
       @open_auth = OpenAuth2.create(
-        @open_auth.authorization_uri = 'https://accounts.google.com/o/oauth2/auth',
-        @open_auth.token_credential_uri = 'https://accounts.google.com/o/oauth2/token',
-        @open_auth.redirect_uri = 'http://wxtest.aggso.com/oauth2callback',
-        @open_auth.client_id = '700678661036-6df2bt5nnlq8bq61sa1s6ji0f2indgc9.apps.googleusercontent.com',
-        @open_auth.client_secret = 'VhUtzfBcK5s27z9rtkaH12Mz',
-        @open_auth.scope = 'https://www.google.com/m8/feeds/'
+        :authorization_uri => 'https://accounts.google.com/o/oauth2/auth',
+        :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+        :redirect_uri => 'http://wxtest.aggso.com/oauth2callback',
+        :client_id => '700678661036-6df2bt5nnlq8bq61sa1s6ji0f2indgc9.apps.googleusercontent.com',
+        :client_secret => 'VhUtzfBcK5s27z9rtkaH12Mz',
+        :scope => 'https://www.google.com/m8/feeds/'
         # @open_auth.save
       )
     end

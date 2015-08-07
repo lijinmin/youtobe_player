62a441edb97740df219d0bd89a0ae0214f96f49c
lijinmin <lijinmin@xjgreat.com>
2015-08-07 10:22:50 +0800

    m

--- app/controllers/open_auth2_controller.rb
+++ app/controllers/open_auth2_controller.rb
@@ -1,5 +1,18 @@
 class OpenAuth2Controller < ApplicationController
+  before_action :new, only:[:oauth]
   def oauth
-    @auth_uri = OpenAuth.get_auth_uri
+    @auth_uri = OpenAuth2.get_auth_uri
+  end
+  def new
+        if OpenAuth2.where(:id=>1).first == nil
+      @open_auth = OpenAuth2.new
+      @open_auth.authorization_uri = 'https://accounts.google.com/o/oauth2/auth'
+      @open_auth.token_credential_uri = 'https://accounts.google.com/o/oauth2/token'
+      @open_auth.redirect_uri = 'http://wxtest.aggso.com/oauth2callback'
+      @open_auth.client_id = '700678661036-6df2bt5nnlq8bq61sa1s6ji0f2indgc9.apps.googleusercontent.com'
+      @open_auth.client_secret = 'VhUtzfBcK5s27z9rtkaH12Mz'
+      @open_auth.scope = 'https://www.google.com/m8/feeds/'
+      @open_auth.save
+    end
   end
 end
--- app/models/open_auth2.rb
+++ app/models/open_auth2.rb
@@ -1,15 +1,15 @@
 class OpenAuth2 < ActiveRecord::Base
   def self.init_open_auth
-    if OpenAuth2.where(:id=>1).first == nil
-      @open_auth = OpenAuth2.new
-      @open_auth.authorization_uri = 'https://accounts.google.com/o/oauth2/auth'
-      @open_auth.token_credential_uri = 'https://accounts.google.com/o/oauth2/token'
-      @open_auth.redirect_uri = 'http://wxtest.aggso.com/oauth2callback'
-      @open_auth.client_id = '700678661036-6df2bt5nnlq8bq61sa1s6ji0f2indgc9.apps.googleusercontent.com'
-      @open_auth.client_secret = 'VhUtzfBcK5s27z9rtkaH12Mz'
-      @open_auth.scope = 'https://www.google.com/m8/feeds/'
-      @open_auth.save
-    end
+    # if OpenAuth2.where(:id=>1).first == nil
+    #   @open_auth = OpenAuth2.new
+    #   @open_auth.authorization_uri = 'https://accounts.google.com/o/oauth2/auth'
+    #   @open_auth.token_credential_uri = 'https://accounts.google.com/o/oauth2/token'
+    #   @open_auth.redirect_uri = 'http://wxtest.aggso.com/oauth2callback'
+    #   @open_auth.client_id = '700678661036-6df2bt5nnlq8bq61sa1s6ji0f2indgc9.apps.googleusercontent.com'
+    #   @open_auth.client_secret = 'VhUtzfBcK5s27z9rtkaH12Mz'
+    #   @open_auth.scope = 'https://www.google.com/m8/feeds/'
+    #   @open_auth.save
+    # end
 
   end
   def self.get_auth_uri
@@ -17,7 +17,7 @@ class OpenAuth2 < ActiveRecord::Base
     auth_uri = (client.authorization_uri(options={})).to_s
   end
   def self.save_oauth_code(code)
-    OpenAuth2.init_open_auth
+    # OpenAuth2.init_open_auth
     open_auth = OpenAuth2.where(id: 1).first
     open_auth.code = code
     open_auth.save
